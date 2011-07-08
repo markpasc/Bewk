0cba0dd06b9a2e9191b7d343cc6def09ea53d13d
Mark Paschal <markpasc@markpasc.org>
2011-07-07 17:42:40 -0700

    Initial Commit

---
+++ Bookish/Book.h
@@ -0,0 +1,13 @@
+//
+//  Book.h
+//  Bookish
+//
+//  Created by Mark Paschal on 7/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Book : NSDocument
+
+@end
---
+++ Bookish/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Bookish
+//
+//  Created by Mark Paschal on 7/7/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
